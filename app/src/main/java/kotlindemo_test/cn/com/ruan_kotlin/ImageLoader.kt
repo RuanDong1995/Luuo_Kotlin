9e0c9068334480fa6a9a9e9cdb072881421abf26
ghx <772648716@qq.com>
2018-11-15 17:59:28 +0800

    第二次提交

---
+++ app/src/main/java/kotlindemo_test/cn/com/ruan_kotlin/ImageLoader.kt
@@ -0,0 +1,10 @@
+package kotlindemo_test.cn.com.ruan_kotlin
+
+/**
+ * @packagename kotlindemo_test.cn.com.ruan_kotlin
+ * @details
+ * @date 2018/11/15
+ * @author ruandong
+ */
+class ImageLoader {
+}
